e408bb7e7e1aefb10d5b0e355a7f8d645c7a073d
Rober Guerrero Mendoza <rober.guerrero@ucb.edu.bo>
2025-03-25 19:29:21 -0400

    add database connection string

---
+++ webapi/src/Core/BovIQ.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BovIQ.Persistence;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : IdentityDbContext(options);
---
+++ webapi/src/Core/BovIQ.Persistence/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BovIQ.Persistence;
+
+public static class DependencyInjection
+{
+    private const string _sectionName = "DataBase";
+    public static IServiceCollection AddDatabaseProvider(
+        this IServiceCollection services,
+        IConfiguration configuration) 
+        => services.AddSqlServer<ApplicationDbContext>(configuration.GetConnectionString(_sectionName));
+}
