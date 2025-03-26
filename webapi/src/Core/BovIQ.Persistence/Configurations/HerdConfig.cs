09ef630f7bcc82525f32a352d11114d87d251e25
Rober Guerrero Mendoza <rober.guerrero@ucb.edu.bo>
2025-03-26 10:25:22 -0400

    set up the domain entities

---
+++ webapi/src/Core/BovIQ.Persistence/Configurations/HerdConfig.cs
@@ -0,0 +1,28 @@
+﻿using BovIQ.Domain.Entities;
+using BovIQ.Persistence.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BovIQ.Persistence.Configurations;
+
+internal sealed class HerdConfig : IEntityTypeConfiguration<Herd>
+{
+    public void Configure(EntityTypeBuilder<Herd> builder)
+    {
+        builder.ToTable(TableName.Herd);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(50);
+
+        ConfigureRelationships(builder);
+    }
+    private static void ConfigureRelationships(EntityTypeBuilder<Herd> builder)
+    {
+        builder.HasMany(x => x.Cows)
+            .WithOne(x => x.Herd)
+            .HasForeignKey(x => x.HerdId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
